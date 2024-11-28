a6c89427fc5220279473d61c37e10f193de441f5
Eduardo Fonseca Ribeiro <eduardo10122005@gmail.com>
2024-11-28 12:53:52 -0300

    Pratica01 - exercício 5

---
+++ Pratica01/Exercicio5.java
@@ -0,0 +1,11 @@
+public class Exercicios{
+
+    public static void main(String[] args) {
+        int soma = 0;
+        for (int i = 0; i <= 10; i++){
+            soma = soma + i;
+        }
+        System.out.println(soma);
+    }
+
+}
