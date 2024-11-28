7f3f4986347f7dbe0eac4f4272194507501ef974
Eduardo Fonseca Ribeiro <eduardo10122005@gmail.com>
2024-11-28 12:46:45 -0300

    Pratica01 - exercício 3

---
+++ Pratica01/Exercicio3.java
@@ -0,0 +1,10 @@
+public class Exercicios{
+
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+        }while (++i <= 5);
+    }
+
+}
